f7893b2c750f18f778b60876182054cf4c6875f3
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2023-11-15 00:46:16 -0300

    Add files via upload

--- pongjs/script.js
+++ pongjs/script.js
@@ -1,9 +1,11 @@
 let ctx, p1_y, p2_y, p1_points, p2_points;
-let w = 1000, h = 700, pw = 20, ph = 100, p1_x = 7, p2_x = w - pw - 7;
+let w = 1920, h = 1080, pw = 20, ph = 100, p1_x = 7, p2_x = w - pw - 7;
 let p1_key, p2_key;
 let ballX, ballY, ballX_ori, ballY_ori;
 let gameStarted = false;
-
+let acceleration = 0.1;
+let ballSpeedX = 5; // Velocidade inicial da bola ao longo do eixo X
+let ballSpeedY = 5; // Velocidade inicial da bola ao longo do eixo Y
 
 
 
@@ -11,15 +13,15 @@ function init() {
     let c = document.getElementById("canva");
     ctx = c.getContext("2d");
 
-    p1_y = (h / 2 -50)
-    p2_y = (h / 2 -50)
+    p1_y = (h / 2 - 50)
+    p2_y = (h / 2 - 50)
 
     p1_points = 0
     p2_points = 0
 
     drawStartScreen();
 
-    document.addEventListener("keydown", function screen (event) {
+    document.addEventListener("keydown", function screen(event) {
         if (event.keyCode === 32 && !gameStarted) {
             gameStarted = true;
             setInterval(fps, 1000 / 60); // Definindo quantos quadros em média 16,66 ms
@@ -46,9 +48,9 @@ function el(x, y, w, h, color) { /// definindo os parametros para o draw()
 
 function drawStartScreen() {
     el(0, 0, w, h, "#000"); // Background
-    ctx.font = "30px Arial";
+    ctx.font = "40px Arial";
     ctx.fillStyle = "#fff";
-    ctx.fillText("Pressione a barra de espaço para começar", w / 4, h / 2);
+    ctx.fillText("Pressione a barra de espaço para começar", w / 3 -50 , h / 2);
 }
 
 
@@ -80,9 +82,6 @@ function ball() {
 }
 
 
-
-
-
 function fps() {
 
     if (p1_key == 87 && p1_y > 0) {
@@ -100,11 +99,26 @@ function fps() {
     //Verificação de colisão
     if (ballX + 10 >= p1_x && ballX <= p1_x + pw && ballY + 10 >= p1_y && ballY <= p1_y + ph) {
         ballX_ori = 1;
+        ballSpeedX += ballSpeedX > 0 ? acceleration : -acceleration;
     }
     if (ballX + 10 >= p2_x && ballX <= p2_x + pw && ballY + 10 >= p2_y && ballY <= p2_y + ph) {
         ballX_ori = -1;
+        ballSpeedX += ballSpeedX > 0 ? acceleration : -acceleration;
+
+    }
+    
+
+    
+    if (!gameStarted) {
+        return; // Se o jogo não estiver iniciado, saia da função
     }
 
+    // Adicione a verificação da pontuação aqui
+    if (p1_points >= 5 || p2_points >= 5) {
+        gameStarted = false; // Se a pontuação de qualquer jogador atingir 5, pare o jogo
+        drawStartScreen(); // Desenha a tela inicial
+        return;
+    }
 
     //Colisão do teto e chão
 
@@ -114,6 +128,15 @@ function fps() {
     ballX += 5 * ballX_ori
     ballY += 5 * ballY_ori
 
+    ballSpeedX += ballSpeedX > 0 ? acceleration : -acceleration;
+    ballSpeedY += ballSpeedY > 0 ? acceleration : -acceleration;
+
+    // Restante do seu código fps
+
+    // Verificação de colisão com as paredes
+    if (ballY + 10 >= h || ballY <= 0) {
+        ballSpeedY *= -1; // Inverte a direção se atingir as paredes superior ou inferior
+    }
 
     //pontos
     if (ballX + 10 > w) {
@@ -128,20 +151,22 @@ function fps() {
     draw()
 }
 
-
+let p1_movementDirection = 0;
 
 document.addEventListener("keydown", function (ky) { // Definindo as teclas
     if (ky.keyCode == 87) {
         p1_key = ky.keyCode;    // W
+        p1_movementDirection = -1; // Define a direção para cima
     } else if (ky.keyCode == 83) {
         p1_key = ky.keyCode;    // S
+        p1_movementDirection = 1; // Define a direção para baixo
     } else if (ky.keyCode == 38) {
         p2_key = ky.keyCode;    // CIMA
     } else if (ky.keyCode == 40) {
         p2_key = ky.keyCode;    // BAIXO
     }
+});
 
 
-})
 
 document.addEventListener("DOMContentLoaded", init);
